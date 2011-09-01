5553e4f1b4448bc5caafcb5d9a5bf73b70d9d1b9
Danilo Vladimiro Borek <d4gnu.dev@gmail.com>
2011-08-31 21:09:35 -0300

    New version: 0.0.8

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@
 static const char description[] =
     I18N_NOOP("A KDE 4 Multimedia Player");
 
-static const char version[] = "%{VERSION}";
+static const char version[] = "0.0.8";
 
 int main(int argc, char **argv)
 {
